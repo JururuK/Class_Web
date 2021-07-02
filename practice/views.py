85f93d22a650440b5b86dee340c5ec8bd43688dd
JururuK <darma4545@gmail.com>
2021-07-02 10:13:11 +0900

    7/2 class commit 2nd

--- practice/views.py
+++ practice/views.py
@@ -2,5 +2,7 @@ from django.http import HttpResponse
 from django.shortcuts import render
 
 # Create your views here.
+
+
 def practice(review) :
-    return HttpResponse('Night Review :)')
+    return render(request, 'base.html')
