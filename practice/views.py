e44c0ce7f2300fce3245c2a8ef901ec5fd8b07f5
JururuK <darma4545@gmail.com>
2021-07-13 09:33:08 +0900

    7/13 class exercise 1

--- practice/views.py
+++ practice/views.py
@@ -2,12 +2,20 @@ from django.http import HttpResponse
 from django.shortcuts import render
 
 # Create your views here.
+from practice.models import NewModel
 
 
 def practice(review) :
+
     if review.method == "POST" :
+
+        temp=review.POST.get('input')
+
+        model_instance = NewModel()
+        model_instance.text = temp
+        model_instance.save()
         return render(review, 'practice/exercise.html',
-                      context = {'text':'POST METHOD'})
+                      context = {'model_instance':model_instance})
     else :
         return render(review, 'practice/exercise.html',
-                      context = {'text':'GET METHOD!'})
+                      context = {'text':'아직은 GET'})
