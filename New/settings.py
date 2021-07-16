1f3c837efa9a76e5530f4c40b3e73016f2ffa9c7
JururuK <darma4545@gmail.com>
2021-07-16 09:51:43 +0900

    7/16 class exercise2 login, signup bootstrap

--- New/settings.py
+++ New/settings.py
@@ -52,7 +52,8 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
-    'practice'
+    'practice',
+    'bootstrap4'
 ]
 
 MIDDLEWARE = [
