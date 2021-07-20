2c5498870f58ded5357ec895cbb32dc58cfe42a8
JururuK <darma4545@gmail.com>
2021-07-20 09:29:13 +0900

    7/20 class exercise1 Profileview

--- practice/urls.py
+++ practice/urls.py
@@ -1,14 +1,16 @@
 from django.contrib.auth.views import LoginView, LogoutView
 from django.urls import path
 
-from practice.views import practice, AccountNew
-
+from practice.views import practice, AccountNew, AccountMyProfile
 app_name = 'practice'
+
+
 
 urlpatterns = [
     path('exercise/', practice, name='exercise'),
     path('login/', LoginView.as_view(template_name='practice/login.html'),
          name = 'login'),
     path('logout/', LogoutView.as_view(), name='logout'),
-    path('new/', AccountNew.as_view(), name = 'new')
+    path('new/', AccountNew.as_view(), name = 'new'),
+    path('detail/<int:pk>', AccountMyProfile.as_view(), name = 'detail')
 ]
