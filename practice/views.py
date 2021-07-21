62cef506176e41d3dc0c85d5e6870c3363c8a90d
JururuK <darma4545@gmail.com>
2021-07-21 15:35:42 +0900

    7/21 class exercise1 id authenticated

--- practice/views.py
+++ practice/views.py
@@ -1,6 +1,6 @@
 from django.contrib.auth.forms import UserCreationForm
 from django.contrib.auth.models import User
-from django.http import HttpResponse, HttpResponseRedirect
+from django.http import HttpResponse, HttpResponseRedirect, HttpResponseForbidden
 from django.shortcuts import render
 
 # Create your views here.
@@ -11,21 +11,25 @@ from practice.forms import AccountModifyForm
 from practice.models import NewModel
 
 def practice(review) :
+    if review.user.is_authenticated:
 
-    if review.method == "POST" :
+        if review.method == "POST" :
 
-        temp=review.POST.get('input')
+            temp=review.POST.get('input')
 
-        model_instance = NewModel()
-        model_instance.text = temp
-        model_instance.save()
+            model_instance = NewModel()
+            model_instance.text = temp
+            model_instance.save()
 
-        return HttpResponseRedirect(reverse('practice:exercise'))
+            return HttpResponseRedirect(reverse('practice:exercise'))
 
-    else:
-        data_list = NewModel.objects.all()
-        return render(review, 'practice/exercise.html',
-                      context={'data_list': data_list})
+        else:
+            data_list = NewModel.objects.all()
+            return render(review, 'practice/exercise.html',
+                          context={'data_list': data_list})
+
+    else :
+        return HttpResponseRedirect(reverse('practice:login'))
 
 class AccountNew(CreateView) :
     model = User
@@ -45,8 +49,32 @@ class AccountModify(UpdateView) :
     success_url = reverse_lazy('practice:exercise')
     template_name = 'practice/modify.html'
 
+    def get(self,review,*args,**kwargs):
+        if review.user.is_authenticated and self.get_object() == review.user:
+            return super().get(review,*args,**kwargs)
+        else:
+            return HttpResponseForbidden()
+
+    def post(self,review,*args,**kwargs) :
+        if review.user.is_authenticated and self.get_object() == review.user:
+            return super().post(review,*args,**kwargs)
+        else:
+            return HttpResponseForbidden()
+
 class AccountDelete(DeleteView) :
     model = User
     context_object_name = 'target_user'
     success_url = reverse_lazy('practice:login')
     template_name = 'practice/delete.html'
+
+    def get(self,review,*args,**kwargs):
+        if review.user.is_authenticated and self.get_object() == review.user:
+            return super().get(review,*args,**kwargs)
+        else:
+            return HttpResponseForbidden()
+
+    def post(self,review,*args,**kwargs) :
+        if review.user.is_authenticated and self.get_object() == review.user:
+            return super().post(review,*args,**kwargs)
+        else:
+            return HttpResponseForbidden()
