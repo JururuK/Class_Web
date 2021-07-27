953db59f440aa929868452cea1f1bd93ffbac0b1
JururuK <darma4545@gmail.com>
2021-07-27 09:54:48 +0900

    7/27 class exercise1  Profile Creation class

--- profiles/urls.py
+++ profiles/urls.py
@@ -1,5 +1,7 @@
+from django.urls import path
 
-
+from profiles.views import ProfileCreateView
+app_name = 'profile'
 urlpatterns = [
-
+    path('create/',ProfileCreateView.as_view(),name='new'),
 ]
--- profiles/views.py
+++ profiles/views.py
@@ -1,3 +1,15 @@
 from django.shortcuts import render
 
 # Create your views here.
+from django.urls import reverse_lazy
+from django.views.generic import CreateView
+
+from profiles.forms import ProfileCreationForm
+from profiles.models import Profile
+
+
+class ProfileCreateView(CreateView):
+    model = Profile
+    form_class = ProfileCreationForm
+    success_url = reverse_lazy('practice:exercise')
+    template_name = 'profiles/new.html'
