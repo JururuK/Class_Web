e23adabe986bf382a130b1eed151ec1cfe064201
JururuK <darma4545@gmail.com>
2021-07-28 15:31:06 +0900

    7/28 class exercise2  Profile update

--- profiles/urls.py
+++ profiles/urls.py
@@ -1,7 +1,9 @@
 from django.urls import path
 
-from profiles.views import ProfileCreateView
+from profiles.views import ProfileCreateView, ProfileUpdateView
+
 app_name = 'profile'
 urlpatterns = [
     path('create/',ProfileCreateView.as_view(),name='new'),
+    path('modify/<int:pk>',ProfileUpdateView.as_view(),name='modify')
 ]
--- profiles/views.py
+++ profiles/views.py
@@ -2,7 +2,7 @@ from django.shortcuts import render
 
 # Create your views here.
 from django.urls import reverse_lazy
-from django.views.generic import CreateView
+from django.views.generic import CreateView, UpdateView
 
 from profiles.forms import ProfileCreationForm
 from profiles.models import Profile
@@ -17,3 +17,10 @@ class ProfileCreateView(CreateView):
     def form_valid(self,form):
         form.instance.user = self.request.user
         return super().form_valid(form)
+
+class ProfileUpdateView(UpdateView):
+    model = Profile
+    form_class = ProfileCreationForm
+    context_object_name = 'target_profile'
+    success_url = reverse_lazy('practice:exercise')
+    template_name = 'profiles/modify.html'
