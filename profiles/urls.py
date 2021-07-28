871c249798ff358241ac766d8feef8fddf400af5
JururuK <darma4545@gmail.com>
2021-07-28 16:25:51 +0900

    7/28 class exercise3  Profile id required

--- profiles/urls.py
+++ profiles/urls.py
@@ -4,6 +4,6 @@ from profiles.views import ProfileCreateView, ProfileUpdateView
 
 app_name = 'profile'
 urlpatterns = [
-    path('create/',ProfileCreateView.as_view(),name='new'),
+    path('new/',ProfileCreateView.as_view(),name='new'),
     path('modify/<int:pk>',ProfileUpdateView.as_view(),name='modify')
 ]
--- profiles/views.py
+++ profiles/views.py
@@ -1,12 +1,17 @@
+from django.contrib.auth.decorators import login_required
 from django.shortcuts import render
 
 # Create your views here.
 from django.urls import reverse_lazy
+from django.utils.decorators import method_decorator
 from django.views.generic import CreateView, UpdateView
 
+from profiles.decorators import is_profile_owner
 from profiles.forms import ProfileCreationForm
 from profiles.models import Profile
 
+@method_decorator(login_required,'get')
+@method_decorator(login_required,'post')
 
 class ProfileCreateView(CreateView):
     model = Profile
@@ -17,6 +22,9 @@ class ProfileCreateView(CreateView):
     def form_valid(self,form):
         form.instance.user = self.request.user
         return super().form_valid(form)
+
+@method_decorator(is_profile_owner,'get')
+@method_decorator(is_profile_owner,'post')
 
 class ProfileUpdateView(UpdateView):
     model = Profile
