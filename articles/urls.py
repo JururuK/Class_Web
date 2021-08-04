3ff8b7d106c71bbfbde133625f5a3102ed8e5f22
JururuK <darma4545@gmail.com>
2021-08-04 15:18:54 +0900

    8/3 class exercise3 articles

--- articles/urls.py
+++ articles/urls.py
@@ -1,10 +1,12 @@
 from django.urls import path
 from django.views.generic import TemplateView
 
-from articles.views import CreateArticleView
+from articles.views import CreateArticleView, DetailArticleView, ModifyArticleView
 
 app_name = 'article'
 urlpatterns = [
     path('list/',TemplateView.as_view(template_name='articles/list.html'),name='list'),
     path('upload/',CreateArticleView.as_view(),name='upload'),
+    path('detail/<int:pk>',DetailArticleView.as_view(),name='detail'),
+    path('modify/<int:pk>',ModifyArticleView.as_view(),name='modify'),
 ]
--- articles/views.py
+++ articles/views.py
@@ -1,8 +1,8 @@
 from django.shortcuts import render
 
 # Create your views here.
-from django.urls import reverse_lazy
-from django.views.generic import CreateView
+from django.urls import reverse_lazy, reverse
+from django.views.generic import CreateView, DetailView, UpdateView
 
 from articles.forms import CreateArticleForm
 from articles.models import Article
@@ -11,9 +11,25 @@ from articles.models import Article
 class CreateArticleView(CreateView):
     model = Article
     form_class = CreateArticleForm
-    success_url = reverse_lazy('practice:exercise')
     template_name = 'articles/upload.html'
 
     def form_valid(self,form):
         form.instance.writer = self.request.user
         return super().form_valid(form)
+
+    def get_success_url(self):
+        return reverse('article:detail',kwargs={'pk': self.object.pk})
+
+class DetailArticleView(DetailView):
+    model = Article
+    context_object_name = 'target_article'
+    template_name = 'articles/detail.html'
+
+class ModifyArticleView(UpdateView):
+    model = Article
+    form_class = CreateArticleForm
+    context_object_name = 'target_article'
+    template_name = 'articleapp/modify.html'
+
+    def get_success_url(self):
+        return reverse('article:detail',kwargs={'pk':self.object.pk})
