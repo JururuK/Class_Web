1f0f66ea66e69c5996f6e312365ff6c03d0a2ed8
JururuK <darma4545@gmail.com>
2021-08-04 15:56:35 +0900

    8/3 class exercise3 articles

--- articles/urls.py
+++ articles/urls.py
@@ -1,7 +1,7 @@
 from django.urls import path
 from django.views.generic import TemplateView
 
-from articles.views import CreateArticleView, DetailArticleView, ModifyArticleView
+from articles.views import CreateArticleView, DetailArticleView, ModifyArticleView, DeleteArticleView
 
 app_name = 'article'
 urlpatterns = [
@@ -9,4 +9,5 @@ urlpatterns = [
     path('upload/',CreateArticleView.as_view(),name='upload'),
     path('detail/<int:pk>',DetailArticleView.as_view(),name='detail'),
     path('modify/<int:pk>',ModifyArticleView.as_view(),name='modify'),
+    path('delete/<int:pk>',DeleteArticleView.as_view(),name='delete')
 ]
--- articles/views.py
+++ articles/views.py
@@ -2,7 +2,7 @@ from django.shortcuts import render
 
 # Create your views here.
 from django.urls import reverse_lazy, reverse
-from django.views.generic import CreateView, DetailView, UpdateView
+from django.views.generic import CreateView, DetailView, UpdateView, DeleteView
 
 from articles.forms import CreateArticleForm
 from articles.models import Article
@@ -34,4 +34,8 @@ class ModifyArticleView(UpdateView):
     def get_success_url(self):
         return reverse('article:detail',kwargs={'pk':self.object.pk})
 
-class D
+class DeleteArticleView(DeleteView):
+    model = Article
+    context_object_name = 'target_article'
+    success_url = reverse_lazy('article:list')
+    template_name = 'articles/delete.html'
