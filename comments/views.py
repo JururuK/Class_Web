52577e559422fd7ec2fa82059875bd976c727880
JururuK <darma4545@gmail.com>
2021-08-10 10:42:35 +0900

    8/9 class exercise commentapp

--- comments/views.py
+++ comments/views.py
@@ -13,7 +13,10 @@ class CreateCommentView(CreateView):
     form_class = CreateCommentForm
     template_name = 'comments/new.html'
 
-
+    def form_valid(self,form):
+        form.instance.writer=self.request.user
+        form.instance.article_id=self.request.POST.get('article_pk')
+        return super().form_valid(form)
 
     def get_success_url(self):
         return reverse('article:detail', kwargs={'pk': self.object.article.pk})
