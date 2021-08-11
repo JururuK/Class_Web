5f6768a994a905e21b345f65da83567e441aa478
JururuK <darma4545@gmail.com>
2021-08-11 14:46:22 +0900

    8/9 class exercise commentapp

--- comments/urls.py
+++ comments/urls.py
@@ -1,9 +1,10 @@
 from django.urls import path
 
-from comments.views import CreateCommentView
+from comments.views import CreateCommentView, DeleteCommentView
 
 app_name='comment'
 
 urlpatterns = [
     path('new/',CreateCommentView.as_view(),name='new'),
+    path('delete/<int:pk>',DeleteCommentView.as_view(),name='delete'),
 ]
--- comments/views.py
+++ comments/views.py
@@ -2,7 +2,7 @@ from django.shortcuts import render
 
 # Create your views here.
 from django.urls import reverse
-from django.views.generic import CreateView
+from django.views.generic import CreateView, DeleteView
 
 from comments.forms import CreateCommentForm
 from comments.models import Comment
@@ -18,5 +18,12 @@ class CreateCommentView(CreateView):
         form.instance.article_id=self.request.POST.get('article_pk')
         return super().form_valid(form)
 
+    def get_success_url(self):
+        return reverse('article:detail', kwargs={'pk': self.object.article.pk})
+
+class DeleteCommentView(DeleteView):
+    model = Comment
+    context_object_name = 'target_comment'
+    template_name = 'comments/delete.html'
     def get_success_url(self):
         return reverse('article:detail', kwargs={'pk': self.object.article.pk})
