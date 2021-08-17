9d19874622c0b7a76c356d5712c2d9d3609e112c
JururuK <darma4545@gmail.com>
2021-08-17 10:46:12 +0900

    8/17 class exercise letters

--- letters/urls.py
+++ letters/urls.py
@@ -1,9 +1,10 @@
 from django.urls import path
 
-from letters.views import CreateLetterView, DetailLetterView
+from letters.views import CreateLetterView, DetailLetterView, ListLetterView
 
 app_name='letter'
 urlpatterns = [
     path('upload/',CreateLetterView.as_view(),name='upload'),
     path('detail/<int:pk>',DetailLetterView.as_view(),name='detail'),
+    path('list/',ListLetterView.as_view(),name='list')
 ]
--- letters/views.py
+++ letters/views.py
@@ -4,13 +4,13 @@ from django.shortcuts import render
 # Create your views here.
 from django.urls import reverse_lazy, reverse
 from django.utils.decorators import method_decorator
-from django.views.generic import CreateView, DetailView
+from django.views.generic import CreateView, DetailView, ListView
 
 from letters.forms import CreateLetterForm
 from letters.models import Letter
 
-@method_decorator(login_required,'get')
-@method_decorator(login_required,'post')
+@method_decorator(login_required(login_url=reverse_lazy('practice:login')),'get')
+@method_decorator(login_required(login_url=reverse_lazy('practice:login')),'post')
 class CreateLetterView(CreateView):
     model = Letter
     form_class = CreateLetterForm
@@ -21,3 +21,9 @@ class DetailLetterView(DetailView):
     model = Letter
     context_object_name = 'target_letter'
     template_name = 'letters/detail.html'
+
+class ListLetterView(ListView):
+    model = Letter
+    context_object_name = 'letter_list'
+    template_name = 'letters/list.html'
+    paginate_by = 10
