6141ae35cd17a8b31e4d5666b35769296345b38c
JururuK <darma4545@gmail.com>
2021-08-17 10:25:09 +0900

    8/17 class exercise letters

--- letters/urls.py
+++ letters/urls.py
@@ -1,7 +1,9 @@
 from django.urls import path
 
-from letters.views import CreateLetterView
+from letters.views import CreateLetterView, DetailLetterView
+
 app_name='letter'
 urlpatterns = [
-    path('upload/',CreateLetterView.as_view(),name='upload')
+    path('upload/',CreateLetterView.as_view(),name='upload'),
+    path('detail/<int:pk>',DetailLetterView.as_view(),name='detail'),
 ]
--- letters/views.py
+++ letters/views.py
@@ -1,15 +1,23 @@
+from django.contrib.auth.decorators import login_required
 from django.shortcuts import render
 
 # Create your views here.
-from django.urls import reverse_lazy
-from django.views.generic import CreateView
+from django.urls import reverse_lazy, reverse
+from django.utils.decorators import method_decorator
+from django.views.generic import CreateView, DetailView
 
 from letters.forms import CreateLetterForm
 from letters.models import Letter
 
-
+@method_decorator(login_required,'get')
+@method_decorator(login_required,'post')
 class CreateLetterView(CreateView):
     model = Letter
     form_class = CreateLetterForm
-    success_url = reverse_lazy('article:list')
     template_name = 'letters/upload.html'
+    def get_success_url(self):
+        return reverse('letter:detail',kwargs={'pk':self.object.pk})
+class DetailLetterView(DetailView):
+    model = Letter
+    context_object_name = 'target_letter'
+    template_name = 'letters/detail.html'
