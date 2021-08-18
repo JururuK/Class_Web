c190c74c7dd0595baca85fbf7d8974b100757247
JururuK <darma4545@gmail.com>
2021-08-18 15:46:37 +0900

    8/18 class exercise letters

--- letters/views.py
+++ letters/views.py
@@ -5,7 +5,9 @@ from django.shortcuts import render
 from django.urls import reverse_lazy, reverse
 from django.utils.decorators import method_decorator
 from django.views.generic import CreateView, DetailView, ListView
+from django.views.generic.list import MultipleObjectMixin
 
+from articles.models import Article
 from letters.forms import CreateLetterForm
 from letters.models import Letter
 
@@ -17,10 +19,15 @@ class CreateLetterView(CreateView):
     template_name = 'letters/upload.html'
     def get_success_url(self):
         return reverse('letter:detail',kwargs={'pk':self.object.pk})
-class DetailLetterView(DetailView):
+class DetailLetterView(DetailView,MultipleObjectMixin):
     model = Letter
     context_object_name = 'target_letter'
     template_name = 'letters/detail.html'
+    paginate_by = 10
+
+    def get_context_data(self, **kwargs):
+        article_list = Article.objects.filter(letter=self.object)
+        return super().get_context_data(object_list=article_list,**kwargs)
 
 class ListLetterView(ListView):
     model = Letter
