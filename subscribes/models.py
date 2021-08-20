674d658a0a2433b2d9ce90bfab3967c8f70c533c
JururuK <darma4545@gmail.com>
2021-08-20 14:46:05 +0900

    8/20 class exercise subscribe

--- subscribes/models.py
+++ subscribes/models.py
@@ -1,3 +1,18 @@
 from django.db import models
 
 # Create your models here.
+from django.contrib.auth.models import User
+from django.db import models
+
+# Create your models here.
+from letters.models import Letter
+
+
+class Subscription(models.Model):
+    user = models.ForeignKey(User,on_delete=models.CASCADE,
+                             related_name='subscription',null=False)
+    letter = models.ForeignKey(Letter,on_delete=models.CASCADE,
+                                related_name='subscription',null=False)
+
+    class Meta:
+        unique_together=['user','letter']
