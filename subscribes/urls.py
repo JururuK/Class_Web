9de675964eb7987b76f3ee8082bdad1a9c60e192
JururuK <darma4545@gmail.com>
2021-08-20 15:11:37 +0900

    8/20 class exercise subscribe

--- subscribes/urls.py
+++ subscribes/urls.py
@@ -1,4 +1,8 @@
+from django.urls import path
 
+from subscribes.views import SubscriptionView
+
+app_name='subscribe'
 urlpatterns = [
-
+    path('subscribe/<int:letter_pk>',SubscriptionView.as_view(),name='subscribe')
 ]
--- subscribes/views.py
+++ subscribes/views.py
@@ -1,3 +1,28 @@
+from django.contrib.auth.decorators import login_required
 from django.shortcuts import render
 
 # Create your views here.
+from django.urls import reverse
+from django.utils.decorators import method_decorator
+from django.views.generic import RedirectView
+
+from letters.models import Letter
+from subscribes.models import Subscription
+
+
+@method_decorator(login_required,'get')
+class SubscriptionView(RedirectView):
+
+    def get(self,review,*args,**kwargs):
+        user = review.user
+        letter = Letter.objects.get(pk=kwargs['letter_pk'])
+        subscription = Subscription.objects.filter(user=user,
+                                                   letter=letter)
+        if subscription.exists():
+            subscription.delete()
+        else :
+            Subscription(user=user,letter=letter).save()
+        return super().get(review,*args,**kwargs)
+
+    def get_redirect_url(self, *args, **kwargs):
+        return reverse('letter:detail',kwargs={'pk':kwargs['letter_pk']})
