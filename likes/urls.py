19e80301db448c5dfd3d3b1343cc09c61fa965b1
JururuK <darma4545@gmail.com>
2021-08-24 10:31:46 +0900

    8/24 class exercise like

--- likes/urls.py
+++ likes/urls.py
@@ -1,5 +1,7 @@
+from django.urls import path
 
-
+from likes.views import LikeArticleView
+app_name = 'like'
 urlpatterns = [
-
+    path('article/<int:article_pk>',LikeArticleView.as_view(),name='article_like'),
 ]
--- likes/views.py
+++ likes/views.py
@@ -1,14 +1,16 @@
+from django.contrib.auth.decorators import login_required
 from django.http import HttpResponseRedirect
 from django.shortcuts import render
 
 # Create your views here.
-from django.urls import reverse
+from django.urls import reverse, reverse_lazy
+from django.utils.decorators import method_decorator
 from django.views.generic import RedirectView
 
 from articles.models import Article
 from likes.models import LikeRecord
 
-
+@method_decorator(login_required(login_url=reverse_lazy('practice:login')),'get')
 class LikeArticleView(RedirectView):
     def get(self,review,*args,**kwargs):
         user = review.user
@@ -16,7 +18,7 @@ class LikeArticleView(RedirectView):
         like_record = LikeRecord.objects.filter(user=user,
                                                 article=article)
 
-        if like_record.exists()
+        if like_record.exists():
             return HttpResponseRedirect(reverse('article:detail',kwargs={'pk':kwargs['article_pk']}))
 
         else:
