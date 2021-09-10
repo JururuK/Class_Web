75e7d5fd2e7349d39074bc95f52a5d9ef7a60a84
JururuK <darma4545@gmail.com>
2021-09-10 10:49:25 +0900

    8/27 class exercise

--- New/settings/base.py
+++ New/settings/base.py
@@ -15,32 +15,8 @@ from pathlib import Path
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 from django.urls import reverse_lazy
 
-BASE_DIR = Path(__file__).resolve().parent.parent #최상위 프로젝트 폴더의 경로
+BASE_DIR = Path(__file__).resolve().parent.parent.parent #최상위 프로젝트 폴더의 경로
 
-env_list = dict()
-
-local_env = open(os.path.join(BASE_DIR, '.env'))
-
-while True :
-    line = local_env.readline() #파일의 끝에 도달했을 떄 조건문을 빠져나가라
-    if not line :
-        break
-    line = line.replace('\n','')
-    start = line.find('=')
-    key = line[:start]
-    value = line[start+1:]
-    env_list[key]=value
-
-# Quick-start development settings - unsuitable for production
-# See https://docs.djangoproject.com/en/3.2/howto/deployment/checklist/
-
-# SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = env_list['SECRET_KEY']
-
-# SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
-
-ALLOWED_HOSTS = ['*']
 
 
 # Application definition
@@ -101,13 +77,6 @@ WSGI_APPLICATION = 'New.wsgi.application'
 
 # Database
 # https://docs.djangoproject.com/en/3.2/ref/settings/#databases
-
-DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
-    }
-}
 
 
 # Password validation
